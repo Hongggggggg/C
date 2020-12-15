b1ead72499e50311f655d2332dfb4fab3c895859
Hongggggggg <hongggggggghammer@gmail.com>
2020-12-15 10:47:18 +0800

    -化简公式，优化运行速率

--- Filter/moving_average_filter.c
+++ Filter/moving_average_filter.c
@@ -4,7 +4,9 @@ int moving_average_filter(int x_n, int avg_last, int n)
 {
     int avg = 0;
 
-    avg = ((n - 1) * avg_last + x_n) / n;
+    //avg = ((n - 1) * avg_last + x_n) / n;
+
+    avg = avg_last + (x_n - avg_last) / n;
 
     return avg;
 }
